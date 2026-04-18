881583b458c096bb7f2bf3be243f3efefbff8369
Emilio Sala <emilio_k88@hotmail.com>
2026-04-18 07:49:25 +0200

    ADD: createFeedFollow query

--- src/db/queries/feeds.ts
+++ src/db/queries/feeds.ts
@@ -1,5 +1,5 @@
 import { db } from "../index.js";
-import { feeds, users } from "../schema.js";
+import { feeds, users, feed_follows } from "../schema.js";
 import { eq } from "drizzle-orm";
 
 export async function createFeed(name: string, url: string, userId: string) {
@@ -24,3 +24,25 @@ export async function getFeedsWithUserName() {
 }
 
 export type Feed = typeof feeds.$inferSelect;
+
+export async function createFeedFollow(userId: string, feedId: string) {
+  const [newFeedFollow] = await db
+    .insert(feed_follows)
+    .values({ userId, feedId })
+    .returning();
+
+  const [result] = await db
+    .select({
+      id: feed_follows.id,
+      createdAt: feed_follows.createdAt,
+      updatedAt: feed_follows.updatedAt,
+      feedName: feeds.name,
+      userName: users.name,
+    })
+    .from(feed_follows)
+    .innerJoin(feeds, eq(feed_follows.feedId, feeds.id))
+    .innerJoin(users, eq(feed_follows.userId, users.id))
+    .where(eq(feed_follows.id, newFeedFollow.id));
+
+  return result;
+}
