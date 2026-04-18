727ccf9116bbb6e9778d3d6c540266e8d356a839
Emilio Sala <emilio_k88@hotmail.com>
2026-04-18 07:37:52 +0200

    ADD: queries feed and userById

--- src/db/queries/feeds.ts
+++ src/db/queries/feeds.ts
@@ -1,12 +1,25 @@
 import { db } from "../index.js";
-import { feeds } from "../schema.js";
-// import { eq } from "drizzle-orm";
+import { feeds, users } from "../schema.js";
+import { eq } from "drizzle-orm";
 
 export async function createFeed(name: string, url: string, userId: string) {
   const [result] = await db
     .insert(feeds)
     .values({ name: name, url: url, userId: userId })
     .returning();
+  return result;
+}
+
+export async function getFeeds() {
+  const result = await db.select().from(feeds);
+  return result;
+}
+
+export async function getFeedsWithUserName() {
+  const result = await db
+    .select()
+    .from(feeds)
+    .innerJoin(users, eq(feeds.userId, users.id));
   return result;
 }
 
