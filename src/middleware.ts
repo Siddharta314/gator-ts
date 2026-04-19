a5e22156e97564f24e65a5910365f0d1de7cc161
Emilio Sala <emilio_k88@hotmail.com>
2026-04-19 09:52:51 +0200

    ADD: middleware to get current user and use it in any other command

---
+++ src/middleware.ts
@@ -0,0 +1,24 @@
+import { CommandHandler } from "./commands.js";
+import { User } from "./db/queries/users.js";
+import { config } from "./config.js";
+import { getUserByName } from "./db/queries/users.js";
+
+type UserCommandHandler = (
+  cmdName: string,
+  user: User,
+  ...args: string[]
+) => Promise<void>;
+
+type MiddlewareLoggedIn = (handler: UserCommandHandler) => CommandHandler;
+
+export function middlewareLoggedIn(
+  handler: UserCommandHandler,
+): CommandHandler {
+  return async (cmdName: string, ...args: string[]) => {
+    const user = await getUserByName(config.currentUserName);
+    if (!user) {
+      throw new Error(`User ${config.currentUserName} not found`);
+    }
+    return handler(cmdName, user, ...args);
+  };
+}
